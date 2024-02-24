986d831cd86ef3ccf6db8cf6a26e305c0f865600
estafeeva <stafeeva.ea@mail.ru>
2024-02-24 20:08:05 +0300

    renew Classes

--- Classes.py
+++ Classes.py
@@ -1,33 +1,26 @@
 class Category:
     """Класс для задания категории."""
-    name: str
-    description: str
-    goods: list
-
     # Переменная на уровне класса для подсчета количества категорий
     number_of_categories = 0
 
-    def __init__(self, name, description, goods):
+    def __init__(self, name: str, description: str, goods: list):
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
         self.goods = goods
-        self.goods_quantity = len(goods) #считаем количество продуктов в категории
+        self.goods_quantity = len(goods) #количество товаров в списке одной категории
 
+        """при увеличении количества категорий к переменной 
+        number_of_categories обращаемся от класса"""
         Category.number_of_categories += 1
 
 class Product:
     """Класс для задания продукта."""
-    name: str
-    description: str
-    price: float
-    quantity: int
 
-    def __init__(self, name, description, price, quantity):
+
+    def __init__(self, name: str, description: str, price: float, quantity: int):
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
-
-
